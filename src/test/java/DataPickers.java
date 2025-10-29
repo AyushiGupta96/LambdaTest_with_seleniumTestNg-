c36161d7c68e5ddd3018d65c14708d0cd5f28e3c
Ayushi Gupta <ayushiguota4050@gmail.com>
2025-10-29 16:44:10 -0700

    added test groups

--- src/test/java/DataPickers.java
+++ src/test/java/DataPickers.java
@@ -1,11 +1,24 @@
 import org.testng.annotations.*;
+@Test(groups = "smoke")
 public class DataPickers {
     @Test
     public void datePicker(){
     System.out.println("date picker");
 }
-@Test
+@Test(groups = {"regression","e2e"})
 public void jqueryPicker(){
     System.out.println("jquery date picker");
+    }
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("before ");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("after method ");
+    }
+    @BeforeGroups(groups = {"regression","smoke"})
+    public void beforeGroup(){
+    System.out.println("agter group");
     }
 }
