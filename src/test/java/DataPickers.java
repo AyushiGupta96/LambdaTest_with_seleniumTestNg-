8dd688e9f4dd6d316fba869c69768b8673f54f74
Ayushi Gupta <ayushiguota4050@gmail.com>
2025-10-29 15:55:21 -0700

    adding methods

--- src/test/java/DataPickers.java
+++ src/test/java/DataPickers.java
@@ -1,3 +1,11 @@
+import org.testng.annotations.*;
 public class DataPickers {
-
+    @Test
+    public void datePicker(){
+    System.out.println("date picker");
+}
+@Test
+public void jqueryPicker(){
+    System.out.println("jquery date picker");
+    }
 }
